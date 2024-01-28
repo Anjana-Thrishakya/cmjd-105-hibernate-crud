4c83dbe9f161dd824deb63a2c375289fefa0f289
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-01-28 11:01:22 +0530

    Student entity composite and multivalued attributes

--- src/entity/StudentEntity.java
+++ src/entity/StudentEntity.java
@@ -1,16 +1,21 @@
 package entity;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import entity.embeded.StudentName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,6 +38,14 @@ public class StudentEntity {
 
     @Column(name = "nic", length = 12, nullable = false)
     private String nic;
+
+    @ElementCollection
+    @CollectionTable(
+        name = "student_mobile", 
+        joinColumns = @JoinColumn(name = "student_id"))
+    private List<String> mobile;
+
+    private StudentName studentName;
 
     @CreationTimestamp
     @Column(name = "create_date", nullable = false)
---
+++ src/entity/embeded/StudentName.java
@@ -0,0 +1,24 @@
+package entity.embeded;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Embeddable
+public class StudentName {
+
+    @Column(name = "first_name", length = 100, nullable = false)
+    private String firstName;
+    @Column(name = "last_name", length = 100, nullable = false)
+    private String lastName;
+}
