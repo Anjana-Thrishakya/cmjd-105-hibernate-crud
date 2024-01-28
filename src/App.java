755d8c2946bc5902a6d0c843b32c01125135674f
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-01-28 11:47:43 +0530

    Student save

--- src/App.java
+++ src/App.java
@@ -1,8 +1,27 @@
-import util.SessionFactoryConfiguration;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.StudentEntity;
+import entity.embeded.StudentName;
+import repository.StudentRepository;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        SessionFactoryConfiguration.getInstance().getSession();
+
+        StudentRepository studentRepository = new StudentRepository();
+
+        StudentName studentName = new StudentName("Student 1 First", "Student 1 Last");
+
+        List<String> mobiles = new ArrayList<>();
+        mobiles.add("0778988876");
+        mobiles.add("0778983876");
+
+        StudentEntity entity = new StudentEntity();
+        entity.setMobile(mobiles);
+        entity.setNic("978876543V");
+        entity.setStudentName(studentName);
+        Integer id = studentRepository.saveStudent(entity);
+        System.out.println("Id : " + id);
+
     }
 }
